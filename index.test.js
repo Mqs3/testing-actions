4cb693d22d209003293b3156fbb8bcdaf14b605e
Mqs3 <miqueiasjp@gmail.com>
2023-05-09 11:53:10 -0300

    teste4

--- index.test.js
+++ index.test.js
@@ -1,7 +1,6 @@
 const request = require("supertest");
 const app = require('./server');
 
-
 describe('Test My app server', () => {
     it('should get main route', async ()=> {
         const res = await request(app).get('/')
